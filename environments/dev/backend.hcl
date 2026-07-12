556c81e1323739f15e1df402b15b040528c8b718
Caroline <9904587@prestadorserv.com.br>
2026-07-12 16:32:17 -0300

    Update backend

---
+++ environments/dev/backend.hcl
@@ -0,0 +1,5 @@
+resource_group_name  = "rg-tfstate-dev"
+storage_account_name = "stterraformdev"
+container_name       = "tfstate"
+key                  = "dev.tfstate"
+use_oidc             = true
--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,21 +1,12 @@
 terraform {
-  required_version = ">=1.8.0"
+  required_version = ">= 1.8.0"
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>4.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate-dev"
-    storage_account_name = "stterraformdev"
-    container_name       = "tfstate"
-    key                  = "dev.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
+  backend "azurerm" {}
 }
